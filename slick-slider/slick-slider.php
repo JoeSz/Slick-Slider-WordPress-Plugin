281e82ca16a8e7f4e59a6d07ce16b232e1b7ad8f
JoeSz <ujbeszel@gmail.com>
2019-10-22 21:08:04 +0200

    Update readme

--- slick-slider/slick-slider.php
+++ slick-slider/slick-slider.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Slick Slider
-Plugin URI:  https://wordpress.org/plugins/slick-slider/
+Plugin URI:  https://github.com/JoeSz/Slick-Slider-WordPress-Plugin
 Description: Turn your native WordPress galleries into beautiful sliders. Powered by the awesome “slick” slider.
-Version:     0.5.1
-Author:      Philipp Bammes
+Version:     0.5.2
+Author:      Philipp Bammes, Joe Szalai
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: slick-slider
@@ -14,12 +14,12 @@ Slick Slider is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
- 
+
 Slick Slider is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with Slick Slider. If not, see <http://www.gnu.org/licenses/>.
 */
